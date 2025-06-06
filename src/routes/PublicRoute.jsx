da2ac82cb924c06737f802ba910e5cc31d400c6a
web-dev-sanket <web.sanketkamble@gmail.com>
2025-06-07 00:16:31 +0530

    setting page

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -6,45 +6,31 @@ import Login from "../pages/auth/Login.jsx";
 import Dashboard from "../pages/modules/dashboard/Dashboard.jsx";
 import AddBalance from "../pages/modules/addbalance/Addbalance.jsx";
 import Distributor from "../pages/modules/distributor/Distributor.jsx";
+import Setting from "../pages/modules/setting/Setting.jsx";
+import SettingDetailPage from "../pages/modules/setting/SettingDetailPage.jsx";
 
 const PublicRoute = () => {
   const [activeTab, setActiveTab] = useState("/");
 
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
+      {/* User Management  */}
       <Route
-        path="/"
+        path="/example"
         element={
-          <Login />
+          <Layout activeTab={activeTab} setActiveTab={setActiveTab}>
+            {/* <UserManagementPanel /> */}
+          </Layout>
         }
       />
-        {/* User Management  */}
-        <Route
-          path="/example"
-          element={
-            <Layout
-              activeTab={activeTab}
-              setActiveTab={setActiveTab}
-            >
-              {/* <UserManagementPanel /> */}
-            </Layout>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={<Dashboard/>}
-        />
-        <Route
-          path="/addbalance"
-          element={<AddBalance/>}
-        />
-        <Route
-          path="/distributor"
-          element={<Distributor/>}
-        />
-
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/addbalance" element={<AddBalance />} />
+      <Route path="/distributor" element={<Distributor />} />
+      <Route path="/setting" element={<Setting />} />
+      <Route path="/setting/:section" element={<SettingDetailPage />} />
     </Routes>
-  )
+  );
 };
 
 export default PublicRoute;
