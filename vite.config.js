14e3ddeb36427fe2559fdb499281df72d053479b
Kaushal soni <101040764+HotShot03@users.noreply.github.com>
2025-06-16 13:04:05 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/lotteryy/', // <-- your repo name with slashes
+  base: '/lotteryy/', // <-- must match your repo name!
   plugins: [react()],
 });
