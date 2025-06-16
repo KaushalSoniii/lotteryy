3032cc8268aa268aafa37624f13458f0163ea88d
Kaushal soni <101040764+HotShot03@users.noreply.github.com>
2025-06-16 14:03:20 +0530

    root change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/lotteryy/', // <-- must match your repo name!
+  base: '/', // <-- root for Netlify!
   plugins: [react()],
 });
