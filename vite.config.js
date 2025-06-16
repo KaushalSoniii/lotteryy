45e9b0d2213e80c073072a7c00fb94dcb0158d1d
Kaushal soni <101040764+HotShot03@users.noreply.github.com>
2025-06-16 14:05:49 +0530

    cmt chng

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // <-- root for Netlify!
+  base: '/lotteryy/', // <-- must match your repo name!
   plugins: [react()],
 });
